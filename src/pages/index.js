7b7533b0959f69bb47e7060679c5d0424c8c9af9
Akif Manzoor <akifmanzoor124@gmail.com>
2021-11-16 18:44:55 -0500

    Created first design of website installed tailwindCss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,24 @@
 import * as React from "react"
+import image from '.././images/icon.png'
+
+import "./index.css"
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Cloud</title>
-      <h1>Home Cloud</h1>
-    </main>
+    <div className="flex flex-col h-full bg-yellow-500">
+      <header className="flex text-6xl justify-center h-1/6 pt-2 text-blue-900">HomeCloud</header>
+      <main className="flex flex-row justify-around items-center h-4/6">
+        <div className="flex flex-col items-center">
+          <h2 className="text-5xl font-semibold pb-10">Real Estate Solutions <br /> for you</h2>
+          <button className="w-full py-6 bg-blue-900 border-2 rounded-lg border-black text-3xl text-yellow-200 font-semibold">Request Access</button>
+        </div>
+        <div>
+          <img src={image} alt="" />
+        </div>
+      </main>
+      <footer className="flex justify-center items-end h-1/6 pb-2">HomeCloud</footer>
+    </div>
   )
 }
 
