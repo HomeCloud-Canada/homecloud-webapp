016f06c24c958c1fe13c05f79a0c4e3170e0abe0
Akif Manzoor <akifmanzoor124@gmail.com>
2021-11-16 20:23:16 -0500

    Added an ellipse below property image Added underline for header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
-import image from '.././images/icon.png'
+import propertyImage from '.././images/property_image.webp'
+import ellipse from ".././images/Ellipse 6.png"
 
 import "./index.css"
 
@@ -7,19 +8,22 @@ import "./index.css"
 const IndexPage = () => {
   return (
     <div className="flex flex-col h-full bg-yellow-500">
-      <header className="flex text-6xl justify-center h-1/6 pt-2 text-blue-900">HomeCloud</header>
+      <header className="flex text-6xl justify-center h-1/6 pt-2 text-indigo-900 font-bold italic">HomeCloud</header>
       <main className="flex flex-row justify-around items-center h-4/6">
         <div className="flex flex-col items-center">
-          <h2 className="text-5xl font-semibold pb-10">Real Estate Solutions <br /> for you</h2>
-          <button className="w-full py-6 bg-blue-900 border-2 rounded-lg border-black text-3xl text-yellow-200 font-semibold">Request Access</button>
+          <h2 className="text-5xl font-semibold pb-10 text-indigo-900">Real Estate Solutions <br /> for you</h2>
+          <button className="w-full py-6 bg-indigo-900 border-2 rounded-lg border-black text-3xl text-yellow-200 font-semibold">Request Access</button>
         </div>
-        <div>
-          <img src={image} alt="" />
+        <div className="images">
+          <img src={propertyImage} alt="" />
+          <img className="circ" src={ellipse} alt="" />
         </div>
       </main>
-      <footer className="flex justify-center items-end h-1/6 pb-2">HomeCloud</footer>
+      <footer className="flex justify-center items-end h-1/6 pb-2"></footer>
     </div>
   )
 }
 
 export default IndexPage
+
+
